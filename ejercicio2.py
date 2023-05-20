624f1d60326fd78a47d76142211ce24691721431
nattkmm <129527144+nattkmm@users.noreply.github.com>
2023-05-20 17:40:05 -0400

    Update ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+filas = np.random.randint(1, 9)
+
+matriz = np.array([[2,3],[4,5]])
+
+matriz_inversa = np.linalg.inv(matriz)
+
+print(matriz_inversa)
+
+matriz_multiplicada = np.multiply(matriz, matriz_inversa)
+
+print(matriz_multiplicada)
