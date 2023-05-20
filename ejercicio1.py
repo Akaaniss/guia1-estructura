c5c92c989c9402d7019414759a8354400c055a54
Akaaniss <annaismonserrat.skz@gmail.com>
2023-05-20 16:21:28 -0400

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,15 @@
 #creacion de matriz
-matriz=[[2,4,6,8,10]
-        [12,14,16,18,20]
-        [22,24,26,28,30]
-        [32,24,26,28,40]
+matriz=[[2,4,6,8,10],
+        [12,14,16,18,20],
+        [22,24,26,28,30],
+        [32,24,26,28,40],
         [42,44,46,48,50]]
+
+suma_mas_alta= 0
+
+for columas in range(5):
+    sumas_columnas = 0
+    for filas in range(5):
+        sumas_columnas += matriz[filas][columas]
+        if sumas_columnas > suma_mas_alta:
+            suma_mas_alta = sumas_columnas
